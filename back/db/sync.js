c75f604709e9df8d086354db7b4522e86cad0f2c
daria_hk <honcharuk.daria95@gmail.com>
2025-07-10 23:14:05 +0200

    add files

---
+++ back/db/sync.js
@@ -0,0 +1,23 @@
+import { User, UserFollow } from "./models/users.js";
+import { Testimonial } from "./models/testimonials.js";
+import { Category } from "./models/categories.js";
+import { Area } from "./models/areas.js";
+import { Ingredient } from "./models/ingredients.js";
+import { Recipe, RecipeIngredient } from "./models/recipes.js";
+
+const syncCondition = { alter: true, force: false };
+const doSync = true;
+
+if (doSync) {
+  await User.sync(syncCondition);
+  await UserFollow.sync(syncCondition);
+  await Testimonial.sync(syncCondition);
+  await Category.sync(syncCondition);
+  await Area.sync(syncCondition);
+  await Ingredient.sync(syncCondition);
+  await Recipe.sync(syncCondition);
+  await RecipeIngredient.sync(syncCondition);
+}
+
+await Category.create({ name: "main" });
+await Area.create({ name: "europe" });
